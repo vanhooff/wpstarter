bb56ab95701bdb6579b049e2cac842c8e33a746f
vanhooff <mail@sandervanhooff.com>
2025-01-29 17:44:55 +0100

    Added some default config to tailwind.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 const typography = require('@tailwindcss/typography');
 const forms = require('@tailwindcss/forms');
 
 const config = {
   content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
+    fontFamily: {
+      body: ['Body', ...defaultTheme.fontFamily.sans],
+      heading: ['Heading', ...defaultTheme.fontFamily.serif],
+    },
+    screens: {
+      xxxs: "340px",
+      xxs: "360px",
+      xs: "480px",
+      sm: "640px",
+      md: "769px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1600px",
+      "3xl": "1920px",
+    },
     extend: {
-      colors: {}, // Extend Tailwind's default colors
+      colors: { // Add desired theme colors here
+        gray: colors.neutral,
+      },
+      typography: {
+        DEFAULT: {
+          css: {},
+        },
+      },
     },
   },
   plugins: [
